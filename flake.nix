67f8ba75c1b79fcfa7c1ff8da58b4944f9c933f3
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2025-06-16 13:23:32 -0600

    removing dependency on nixpkgs (can use caller's nixpkgs)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Reusable development check definitions with Nix-based execution framework";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     blueprint.url = "github:numtide/blueprint";
   };
 
